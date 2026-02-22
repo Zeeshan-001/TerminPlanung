d50f183546782c2832c2351ec27d9e54d468063f
unknown <fachinformatik1@gmail.com>
2026-02-22 14:52:35 +0100

    LazyLoading

---
+++ src/features/calendar/pages/CalendarPage.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import type { JSX } from "react";
+
+const Calendar = (): JSX.Element => {
+  return <p>Calendar</p>;
+};
+
+export default Calendar;
---
+++ src/features/dashboard/pages/DashboardPage.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import type { JSX } from "react";
+
+const Dashboard = (): JSX.Element => {
+  return <p>Dashboard</p>;
+};
+
+export default Dashboard;
